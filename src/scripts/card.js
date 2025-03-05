555289a3e46080f247a2a45fd7a61cb192104354
Vladislav <vladislavrozhkov1996@yandex.ru>
2025-03-05 13:23:49 +0500

    one

---
+++ src/scripts/card.js
@@ -0,0 +1,37 @@
+export function toggleLike(event) {
+  event.target.classList.toggle("card__like-button_is-active");
+}
+
+export function buildCard(
+  cardData,
+  handleCardDeleteButtonClick,
+  handleCardImageClick,
+  handleCardLikeButtonClick
+) {
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const imageElement = cardElement.querySelector(".card__image");
+  imageElement.src = cardData.link;
+  imageElement.alt = cardData.name;
+
+  imageElement.addEventListener("click", () => handleCardImageClick(cardData));
+
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  cardTitleElement.textContent = cardData.name;
+
+  const likeButton = cardElement.querySelector(".card__like-button");
+  likeButton.addEventListener("click", handleCardLikeButtonClick);
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    handleCardDeleteButtonClick(cardElement);
+  });
+
+  return cardElement;
+}
+
+export function removeCard(cardElement) {
+  cardElement.remove();
+}
---
+++ src/scripts/modal.js
@@ -0,0 +1,28 @@
+import { validationConfig } from "./index.js";
+import { clearValidation } from "./validation.js";
+
+export function openPopup(popupElement) {
+  popupElement.classList.add("popup_is-opened");
+  document.addEventListener("keydown", closeOnEscape);
+}
+
+export function closePopup(popupElement) {
+  clearValidation(popupElement.querySelector(".popup__form"), validationConfig);
+  popupElement.classList.remove("popup_is-opened");
+  document.removeEventListener("keydown", closeOnEscape);
+}
+
+export function closeOnEscape(event) {
+  if (event.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_is-opened");
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
+
+export function closePopupOnOverlayClick(event) {
+  if (event.target.classList.contains("popup")) {
+    closePopup(event.target);
+  }
+}
